2c06d308cf3e3c69e13f9c807f3ea4c25c9e4292
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-07-19 19:54:28 +0100

    Remove functions that load outdated txt formats
    This has been replaced by XML files.

--- arm9/source/GfxEngine/Script/Script_Load.cpp
+++ arm9/source/GfxEngine/Script/Script_Load.cpp
@@ -78,23 +78,16 @@ void LoadScript(char filename[])
     ResetCounter();  // for percentbar
     UpdateCounter(); // for percentbar
 
-    char SongFile[40];
-    strcpy(SongFile, "");
-
     // erst schaun ob alles da is
     FILE *xml = fopen(filename, "r");
-    if (xml == NULL) {
-        // Fatal error, cannot load
+    if (xml == NULL)
         Crash("Script not found:\n%s", filename);
-    }
     fclose(xml);
 
     // hier muss es abgehn...
     TiXmlDocument *xmlDoc = new TiXmlDocument(filename);
-    if (!xmlDoc->LoadFile()) {
-        // Fatal error, cannot load
+    if (!xmlDoc->LoadFile())
         Crash("Script invalid:\n%s", filename);
-    }
 
     // search for <map> </map>
     TiXmlElement *map = xmlDoc->FirstChildElement("map");
@@ -108,24 +101,26 @@ void LoadScript(char filename[])
         snprintf(FileNameCom, sizeof(FileNameCom), "/wolveslayer/maps/%s", map->Attribute("file"));
         LoadMap(FileNameCom);
     } else {
-        Crash("->No Mapfile specified:\n%s", filename);
+        Crash("No map file specified:\n%s", filename);
     }
 
     // song
-    if (map->Attribute("music")) {
+    char SongFile[40];
+    if (map->Attribute("music"))
         snprintf(SongFile, sizeof(SongFile), "/wolveslayer/bgfx/%s", map->Attribute("music"));
-    } else {
-        Crash("->No Musikfile specified:\n%s", filename);
-    }
+    else
+        Crash("No music file specified:\n%s", filename);
 
     // environment
     if (map->Attribute("env")) {
         if (strncmp("outside", map->Attribute("env"), 7) == 0)
             outside = true;
-        if (strncmp("inside", map->Attribute("env"), 6) == 0)
+        else if (strncmp("inside", map->Attribute("env"), 6) == 0)
             outside = false;
+        else
+            Crash("Invalid environment specified:\n%s", filename);
     } else {
-        Crash("->No environment specified:\n%s", filename);
+        Crash("No environment specified:\n%s", filename);
     }
 
     // load the grounds
@@ -163,78 +158,3 @@ bool IsMapOutside(void)
 {
     return outside;
 }
-
-#if 0
-static void LoadMapChange(char *Com)
-{
-    // scan out Map-FileName
-    char FileName[40];
-    sscanf(Com, "%*s %*i %*i %39s", FileName);
-
-    // scan out positions
-    int x = 0, y = 0;
-    sscanf(Com, "%*s %i %i", &x, &y);
-
-    // scan out GotTOpositions
-    int tox = 0, toy = 0;
-    sscanf(Com, "%*s %*i %*i %*s %i %i", &tox, &toy);
-
-    // Make a hole path with that
-    char FileNameCom[60];
-    snprintf(FileNameCom, sizeof(FileNameCom), "/wolveslayer/maps/%s", FileName);
-
-    // Here is that final call add the exit
-    AddMapChange(x, y, FileNameCom, tox, toy);
-}
-
-static void LoadMapChangeDoor(char *Com)
-{
-    // Example:
-    //   MapChangeDoor Key0 25 5 Map2.txt 1 6
-
-    // scan out Map-FileName
-    char FileName[40];
-    sscanf(Com, "%*s KEY%*i %*i %*i %39s %*i %*i", FileName);
-
-    // scan out Keynumber
-    int Keynum;
-    sscanf(Com, "%*s KEY%i %*i %*i %*s %*i %*i", &Keynum);
-    if (Keynum > 100 || Keynum < 0)
-        Keynum = 0;
-
-    // scan out positions
-    int x = 0, y = 0;
-    sscanf(Com, "%*s KEY%*i %i %i %*s %*i %*i", &x, &y);
-
-    // scan out GotTOpositions
-    int tox = 0, toy = 0;
-    sscanf(Com, "%*s KEY%*i %*i %*i %*s %i %i", &tox, &toy);
-
-    // Make a hole path with that
-    char FileNameCom[60];
-    snprintf(FileNameCom, sizeof(FileNameCom), "/wolveslayer/maps/%s", FileName);
-
-    // Here is that final call add the exit
-    AddMapDoor(x, y, FileNameCom, tox, toy, Keynum);
-}
-
-static void AddEvent(char *com)
-{
-    // If it is 0 we know that AddText was called after Loading a NPC
-    char *pch;
-    pch = strtok(com, " ");   // First was command "^^addtext"
-    pch = strtok(NULL, "\""); // next is a "
-
-    AddTexttoEvent(pch);
-}
-
-static void AddDoorTXT(char *com)
-{
-    // If it is 0 we know that AddText was called after Loading a NPC
-    char *pch;
-    pch = strtok(com, " ");   // First was command "^^addtext"
-    pch = strtok(NULL, "\""); // next is a "
-
-    AddTexttoDoor(pch);
-}
-#endif

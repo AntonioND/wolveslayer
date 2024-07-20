4642b11e2e71569787b88025f16eee3fcf23d7c7
Antonio Niño Díaz <antonio_nd@outlook.com>
2024-07-20 17:03:17 +0100

    Add comment about unused feature

--- arm9/source/GfxEngine/Script/Script_Load.h
+++ arm9/source/GfxEngine/Script/Script_Load.h
@@ -7,6 +7,7 @@
 void LoadScript(char filename[]);
 
 // This returns true if the map is outdoors, false if it is indoors.
+// TODO: This isn't used in any of the original maps.
 bool IsMapOutside(void);
 
 #endif // GFXENGINE_SCRIPT_SCRIPT_LOAD_H__
